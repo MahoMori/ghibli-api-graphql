17afbe12e6992c59b121f308ee9bd227c5f3781f
MahoMori <tr27e1104@gmail.com>
2022-05-06 08:50:33 -0700

    added content to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
     "./src/components/Card.tsx",
+    "./src/components/Modal.tsx",
     "./src/App.tsx",
-    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
     extend: {},
